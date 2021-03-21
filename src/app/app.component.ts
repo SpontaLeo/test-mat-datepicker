070b20e0b9bb71c07edb3947aa7f0d93961f95c2
Nayuta <ttxmtt@163.com>
2021-03-21 10:52:36 +0800

    mat datepicker format test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import * as moment from 'moment';
+
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test-mat-datepicker';
+  testForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.testForm = this.fb.group({
+      date: [null],
+    });
+  }
+
+  printValue(): void {
+    const value = this.testForm.controls.date.value;
+    console.log(value, moment.isMoment(value));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,65 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import * as moment from 'moment';
 
-import { AppRoutingModule } from './app-routing.module';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  MatNativeDateModule,
+} from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import { NgModule } from '@angular/core';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: [
+      'DD-MM-YYYY',
+      'YYYY-MM-DD',
+      'YYYY/MM/DD',
+      'DD/MM/YYYY',
+      'YYYY-MM-DDTHH:mm:ss:sss Z',
+    ],
+  },
+  display: {
+    dateInput: 'DD-MM-YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE],
+    },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: MY_FORMATS,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
